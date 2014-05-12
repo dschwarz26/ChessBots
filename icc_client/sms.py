be52fb094c8a5a9525d23710ff2d22680daaa72b
dschwarz <danielschwarz26@gmail.com>
2014-05-11 20:36:20 -0700

    Update sms to T-Mobile

--- icc_client/sms.py
+++ icc_client/sms.py
@@ -7,8 +7,13 @@ class SMS:
 		self.email = 'danielschwarz26@gmail.com'
 		self.phone_number = '9167170998'
 
-	#Works only for AT&T phones.
+	#Works only for T-Mobile phones.
 	def send_sms(self, message, number=None):
 		if not number:
 			number = self.phone_number
-		mail.send_mail(self.email, self.phone_number + '@mms.att.net', 'ICC Notification!', message)
+		mail.send_mail(
+        self.email,
+        self.phone_number + '@tmomail.net',
+        'ICC Notification!',
+        message
+    )
